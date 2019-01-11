f4efed823e622849f7262faa2f3a3450ceee7795
Dmitry Zaguzin <cindersstone@gmail.com>
2019-01-11 19:44:21 +0800

    тест

--- src/ecscurs.js
+++ src/ecscurs.js
@@ -6,7 +6,7 @@ export default class ecscurs extends React.Component {
     return (
       <div class = "boxm">
         <div class = "box">
-            <h2 class = "tit2">Экскурсий по КБЖД на 2018 год</h2>
+            <h2 class = "tit2">Экскурсий по КБЖД на 2018 г.</h2>
         </div>          
         
       </div>
