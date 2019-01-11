e72fd0829b66f6fdb8941fe865e43aef194ea076
Dmitry Zaguzin <cindersstone@gmail.com>
2019-01-11 20:44:53 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/Baikal-Project/precache-manifest.5400457ae3d9fbf1aa20b1946f25afb4.js"
+  "/baikal-project/precache-manifest.939e9a272c5c02323648cc25e2454dd0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Baikal-Project/index.html", {
+workbox.routing.registerNavigationRoute("/baikal-project/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
