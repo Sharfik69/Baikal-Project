a52d1f74e4612da8b21da612571337c79e6d1b7e
Dmitry Zaguzin <cindersstone@gmail.com>
2019-01-11 20:17:36 +0800

    Updates

---
+++ precache-manifest.5400457ae3d9fbf1aa20b1946f25afb4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9325eb136065bede8e9b",
+    "url": "/Baikal-Project/static/js/runtime~main.9325eb13.js"
+  },
+  {
+    "revision": "9c2c2b28e7c1f555d150",
+    "url": "/Baikal-Project/static/js/main.9c2c2b28.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/Baikal-Project/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "9c2c2b28e7c1f555d150",
+    "url": "/Baikal-Project/static/css/main.20aca787.chunk.css"
+  },
+  {
+    "revision": "d1baae95683933a83df30b105ee0518d",
+    "url": "/Baikal-Project/index.html"
+  }
+];
